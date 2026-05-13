66b42b59fdc2c6c18800793ff4716f57073324e0
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-05-13 15:01:49 -0700

    Assignment added

---
+++ asigment2.py
@@ -0,0 +1,8 @@
+#write a programe that checks if a number is negative/positive or zero
+num = float(input("enter a number: "))
+if num > 0:
+    print("positive number")
+elif num == 0:
+    print("zero")
+else:
+    print("negative number")
--- asinment1.py
+++ asinment1.py
@@ -9,3 +9,11 @@ value1 = 12
 value2 = 13
 sum = value1 + value2
 print("result is:",sum)
+
+
+
+#that two values are
+value1 = 50
+value2 = 25
+sum = value1 + value2
+print("sum")
