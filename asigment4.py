ebf41417165f13f1bc8955a330b0f3c042c3f9cc
Alijaun514 <muhammadalijaun0514@gmail.com>
2026-05-20 16:15:06 -0700

    asigment added

---
+++ asigment4.py
@@ -0,0 +1,14 @@
+#while loop
+var = 1
+while var < 6:
+    print(var)
+    if var == 4:
+        break
+    var += 1
+
+
+    # while loop
+    count = 0
+    while count < 6:
+        print(count,"python")
+        count =  count + 1
